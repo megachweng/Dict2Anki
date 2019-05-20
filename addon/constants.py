49b14335ce6dec227da6e28c172c01f5740c0139
sidlly <shuang.chweng@welian.com>
2019-05-20 13:18:55 +0800

    从template中读取卡片模板

--- addon/constants.py
+++ addon/constants.py
@@ -1,6 +1,9 @@
+import os
+
 VERSION = 'v7.0.0'
 MODEL_VERSION = 'v1.0.0'
 MODEL_NAME = f'Dict2AnkiModel-{MODEL_VERSION}'
 RELEASE_URL = 'https://github.com/megachweng/Dict2Anki'
 VERSION_CHECK_API = 'https://api.github.com/repos/megachweng/Dict2Anki/releases/latest'
 MODEL_FIELDS = ['term', 'content', 'image', 'Pron']
+ADDON_PATH = os.path.abspath(os.path.dirname(__file__))
--- addon/noteManager.py
+++ addon/noteManager.py
@@ -1,8 +1,8 @@
 import json
-
+import os
 import anki
 from aqt import mw
-from .constants import MODEL_FIELDS
+from .constants import MODEL_FIELDS, ADDON_PATH
 import logging
 
 logger = logging.getLogger('dict2Anki.noteManager')
@@ -60,207 +60,15 @@ def getOrCreateModelCardTemplate(modelObject, cardTemplateName):
     if cardTemplateName in [t.get('name') for t in existingCardTemplate]:
         return
     cardTemplate = mw.col.models.newTemplate(cardTemplateName)
+    with open(os.path.join(ADDON_PATH, 'template', 'front.html'), encoding='utf-8') as fp:
+        cardTemplate['qfmt'] = fp.read()
 
-    cardTemplate['qfmt'] = '''
-    <div>
-    <img id='image' style="max-height: 140px" src="">
-    <h1 id="term">{{term}}</h1>
-    <div class="section">
-    <span class="pron"><span style="color: #777">美</span> <span id="AmEPhonetic"></span></span>
-    <span class="pron"><span style="color: #777">英</span> <span id="BrEPhonetic"></span></span>
-    </div>
-    <div id='definitionSection' class="section" style="clear: left">
-    <ul id="definitionList"></ul>
-    </div>
-    </div>
+    with open(os.path.join(ADDON_PATH, 'template', 'back.html'), encoding='utf-8') as fp:
+        cardTemplate['afmt'] = fp.read()
 
-    <div class="section clear" style="width: 100%">
-    <hr>
-    </div>
-    
-    <div class="section clear">
-    <ul id="phraseList"></ul>
-    </div>
+    with open(os.path.join(ADDON_PATH, 'template', 'card.css'), encoding='utf-8') as fp:
+        modelObject['css'] = fp.read()
 
-    <hr>
-    
-    <div class="section clear">
-    <ul id="sentenceList"></ul>
-    </div>
-    {{Pron}}
-
-    
-    <data id="jsonString" style="display: none">{{content}}</data>
-    <script>
-        var jsonString = document.getElementById('jsonString').innerText
-        var queryResult = JSON.parse(jsonString)
-        document.getElementById('AmEPhonetic').innerText = '[' + queryResult.AmEPhonetic + ']'
-        document.getElementById('BrEPhonetic').innerText = '[' + queryResult.BrEPhonetic + ']'
-        var phraseUl = document.getElementById('phraseList')
-        var sentenceUl = document.getElementById('sentenceList')
-        addToUl(phraseUl, queryResult.phrase)
-        addToUl(sentenceUl, queryResult.sentence)
-    
-        function addToUl(whichUl, dataList) {
-            for (var i = 0; i < dataList.length; i++) {
-                var li = document.createElement('li')
-                var pf = document.createElement('p')
-                pf.className = 'front'
-                pf.innerText = dataList[i][0]
-                li.appendChild(pf)
-                whichUl.appendChild(li)
-            }
-        }
-    </script>    
-    '''
-
-    cardTemplate['afmt'] = '''
-    <div>
-    <img id='image' style="max-height: 140px" src="{{image}}">
-    <h1 id="term">{{term}}</h1>
-    <div class="section">
-    <span class="pron"><span style="color: #777">美</span> <span id="AmEPhonetic"></span></span>
-    <span class="pron"><span style="color: #777">英</span> <span id="BrEPhonetic"></span></span>
-    </div>
-    <div id='definitionSection' class="section" style="clear: left">
-    <ul id="definitionList"></ul>
-    </div>
-    </div>
-
-    <div class="section clear" style="width: 100%">
-    <hr>
-    </div>
-    
-    <div class="section clear">
-    <ul id="phraseList"></ul>
-    </div>
-
-    <hr>
-    
-    <div class="section clear">
-    <ul id="sentenceList"></ul>
-    </div>
-    {{Pron}}
-    
-    <data id="jsonString" style="display: none">{{content}}</data>
-    <script>
-        var jsonString = document.getElementById('jsonString').innerText
-        var queryResult = JSON.parse(jsonString)
-        document.getElementById('AmEPhonetic').innerText = '[' + queryResult.AmEPhonetic + ']'
-        document.getElementById('BrEPhonetic').innerText = '[' + queryResult.BrEPhonetic + ']'
-        imgElement = document.getElementById('image') 
-        if (Boolean(queryResult.image)){
-            imgElement.setAttribute('src', queryResult.image)
-        }else{
-            imgElement.parentNode.removeChild(imgElement)
-        }
-        document.getElementById('image').setAttribute('src', queryResult.image)
-        var definitionUl = document.getElementById('definitionList')
-        for (var i = 0; i < queryResult.definition.length; i++) {
-            var node = document.createElement('li')
-            node.className = 'def'
-            node.innerText = queryResult.definition[i]
-            definitionUl.appendChild(node)
-        }
-        var phraseUl = document.getElementById('phraseList')
-        var sentenceUl = document.getElementById('sentenceList')
-        addToUl(phraseUl, queryResult.phrase)
-        addToUl(sentenceUl, queryResult.sentence)
-    
-        function addToUl(whichUl, dataList) {
-            for (var i = 0; i < dataList.length; i++) {
-                var li = document.createElement('li')
-                var pf = document.createElement('p')
-                var pb = document.createElement('p')
-                pf.className = 'front'
-                pf.innerText = dataList[i][0]
-                pb.className = 'back'
-                pb.innerText = dataList[i][1]
-    
-                li.appendChild(pf)
-                li.appendChild(pb)
-                whichUl.appendChild(li)
-            }
-        }
-    </script>
-    '''
-
-    modelObject['css'] = '''
-        .card {
-            font-family: arial;
-            font-size: 20px;
-            text-align: left;
-            color: black;
-            background-color: white;
-        }
-         #term {
-            padding-bottom: 10px;
-            padding-left: 10px;
-            margin: 0;
-        }
-        .pos {
-            width: 35px;
-            font-size: 93%;
-            background-color: #aaa;
-            color: #fff;
-            line-height: 18px;
-            vertical-align: middle;
-            text-align: center;
-            float: left;
-            font-weight: bold;
-        }
-        .def {
-            line-height: 20px;
-            vertical-align: top;
-            font-size: 14px;
-            color: #333;
-            font-weight: bold;
-        }
-        ul {
-            clear: both;
-            padding-top: 10px;
-            border: 0;
-            border-collapse: collapse;
-            border-spacing: 0;
-            list-style: none;
-            margin: auto;
-            padding: 0;
-        }
-        li {
-            text-align: left;
-            padding-left: 10px;
-            border-left: .25em solid #dfe2e5;
-        }
-        image {
-            display: inline;
-            float: right;
-            clear: both;
-        }
-        .section {
-            margin-top: 10px;
-            float: left;
-        }
-        .clear {
-            clear: both;
-        }
-        .pron {
-            margin-right: 5px;
-            padding-left: 5px;
-            float: left;
-        }
-        .back {
-            /* padding: 10px; */
-            margin-bottom: 20px;
-            margin-top: -15px;
-            color: #777;
-        }
-        hr {
-            clear: left;
-        }
-        p {
-            margin-top: 0px;
-        }
-    '''
     mw.col.models.addTemplate(modelObject, cardTemplate)
 
 
