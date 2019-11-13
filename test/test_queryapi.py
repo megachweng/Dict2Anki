b5b49a55d00ad8077612fba027b15e3afbbccd31
wd <wd@wdicc.com>
2019-11-13 14:41:43 +0800

    Add eudict query api

---
+++ test/test_queryapi.py
@@ -0,0 +1,26 @@
+from addon.queryApi.eudict import API
+
+api = API()
+
+keys = ('term', 'definition', 'phrase', 'image', 'sentence', 'BrEPhonetic', 'AmEPhonetic', 'BrEPron', 'AmEPron')
+def check_result(res):
+    ret = []
+    for key in keys:
+        if not res.get(key):
+            ret.append(key)
+    return ret
+
+def test_eudict_no_phrase_and_image():
+    res = api.query('stint')
+    ret = check_result(res)
+    assert set(ret) - set(['image', 'phrase']) == set()
+
+def test_eudict_with_all():
+    res = api.query('flower')
+    ret = check_result(res)
+    assert set(ret) == set()
+
+def test_eudict_with_none():
+    res = api.query('asafesdf')
+    ret = check_result(res)
+    assert set(ret) - set(keys) == set()
