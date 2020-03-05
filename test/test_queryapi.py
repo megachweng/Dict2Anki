b9157c3dbe6f92c6c2cfc38ad7720bc6ebbf1b57
megachweng <megachweng@gmail.com>
2020-03-05 14:16:53 +0800

    update test

--- test/test_queryapi.py
+++ test/test_queryapi.py
@@ -1,8 +1,13 @@
+import os
+
 from addon.queryApi.eudict import API
+import pytest
 
 api = API()
 
 keys = ('term', 'definition', 'phrase', 'image', 'sentence', 'BrEPhonetic', 'AmEPhonetic', 'BrEPron', 'AmEPron')
+
+
 def get_missing_fileds_set(res):
     ret = []
     for key in keys:
@@ -10,21 +15,25 @@ def get_missing_fileds_set(res):
             ret.append(key)
     return set(ret)
 
+
 def test_eudict_no_phrase_and_image():
     res = api.query('stint')
     ret = get_missing_fileds_set(res)
     expect = set(['image', 'phrase'])
     assert ret == expect
 
+
 def test_eudict_with_all():
     res = api.query('flower')
     ret = get_missing_fileds_set(res)
     assert ret == set()
 
+
 def test_eudict_with_none():
     res = api.query('asafesdf')
     ret = get_missing_fileds_set(res)
     assert ret == set(keys) - set(['term'])
+
 
 def test_eudict_implication():
     # 不包含图片，定义不在正常规则内，包含 trans
@@ -33,6 +42,7 @@ def test_eudict_implication():
     expect = set(['image'])
     assert ret == expect
 
+
 def test_eudict_epitomize():
     # 不包含图片，定义不在正常规则内
     res = api.query('epitomize')
@@ -40,11 +50,13 @@ def test_eudict_epitomize():
     expect = set(['image', 'phrase'])
     assert ret == expect
 
+
 def test_eudict_periodical():
     # 包含图片，定义不在正常规则内
     res = api.query('periodical')
     ret = get_missing_fileds_set(res)
     assert ret == set()
+
 
 def test_eudict_divisional():
     # 又一种特殊情况，只有一个音标
@@ -53,6 +65,8 @@ def test_eudict_divisional():
     expect = set(['image', 'AmEPhonetic'])
     assert expect == ret
 
+
+@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", reason="Travis中查询结果没有image字段")
 def test_eudict_image_url_without_https():
     res = api.query('gelatin')
-    assert res['image'][0:8] == 'https://'
+    assert res['image'].startswith('https://')
